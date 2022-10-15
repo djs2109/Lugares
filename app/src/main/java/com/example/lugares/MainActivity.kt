6a9ff09dd573c775dc318cbd7a0d36435e1e57da
djimenez80640 <djimenez80640@ufide.ac.cr>
2022-10-14 20:14:46 -0600

    Cuarto Commit

--- app/src/main/java/com/example/lugares/MainActivity.kt
+++ app/src/main/java/com/example/lugares/MainActivity.kt
@@ -11,8 +11,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import java.time.LocalDate
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
